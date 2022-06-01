0534ccbd7ca5837da71de573d99032c696ea2c67
momoka kawaguchi <momojazz@gmail.com>
2022-06-01 22:21:28 +0900

    Co-authored-by: Masato Ebisawa <j0td@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+
+
+const Todo = (props) => {
+  return (
+    <StyledArticle>
+      <input type="checkbox" />
+      <label>{ props.title }</label>
+    </StyledArticle>
+  )
+};
+
+const TodoList = (props) => {
+  return (
+    <StyledArticle>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+      <Todo title="インターンの申し込み" />
+      <Todo title="aaa" />
+      <Todo title="aaa" />
+      <Todo title="aaa" />
+      <Todo title="aaa" />
+      <Todo title="aaa" />
+      <Todo title="aaa" />
+    </StyledArticle>
+  )
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Todoリスト⭐️</h1>
+      <TodoList
+        title="今週やること！！"
+        content="greinugewjiutj94iu3wgj9043wg9j4j943w"
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+const StyledArticle = styled.article`
+  background-color: #fff;
+  padding: 24px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  margin-bottom: 16px;
+
+  h2 {
+    font-size: 24px;
+    color: rgba(0, 0, 0, 0.9);
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 16px;
+    color: rgba(0, 0, 0, 0.9);
+  }
+`;
